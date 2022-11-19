74cec7021f4450a91aee61b550853272fd30a338
CinemaProject <huudang23102001@gmail.com>
2022-11-19 19:14:37 +0700

    useState student

--- src/component/dang/dang.js
+++ src/component/dang/dang.js
@@ -1,5 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import './dang.css';
 export const Dang =()=>{
-    return (
-        <h1>Dang</h1>
+    const [students, setStudents] = useState([{name : 'Dang' , age: 21},
+                                                {name: 'Hoang', age: 22},
+                                                {name: 'Nam', age: 22},
+                                                {name: 'Tuan', age: 22},
+                                                {name: 'Hoang', age: 22}]);
+
+
+    return ( 
+    <div className="student">
+                <table>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+        </tr>
+        {students.map( item => {
+          return (
+            <tr>
+              <td>{item.name}</td>
+              <td>{item.age}</td>
+            </tr>
+          )
+        })}
+      </table>
+    </div>
     )
-}
+};
