2ef7290aa5a44996fb10fb5be0ecbd796924d601
CinemaProject <huudang23102001@gmail.com>
2022-11-20 00:21:40 +0700

    update input student

--- src/component/dang/dang.js
+++ src/component/dang/dang.js
@@ -1,16 +1,55 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState } from 'react';
 import './dang.css';
 export const Dang =()=>{
-    const [students, setStudents] = useState([{name : 'Dang' , age: 21},
-                                                {name: 'Hoang', age: 22},
-                                                {name: 'Nam', age: 22},
-                                                {name: 'Tuan', age: 22},
-                                                {name: 'Hoang', age: 22}]);
-
-
+  const [listName, setName] = useState("");
+  const [listAge,setAge] = useState("");
+  const [students, setStudents] = useState([{name : 'Dang' , age: '21'},{name: 'Hoang', age: '22'}, {name: 'Nam', age: '22'},{name: 'Tuan', age: '22'},{name: 'Hoang', age: '22'}]);
+    
+    const handletextChange = (e) => {
+    setName(e.currentTarget.value);
+    console.log(e.currentTarget.value);
+    }
+    const handlenumChange = (e) => {
+      e.preventDefault();
+      setAge(e.currentTarget.value);
+      console.log(e.currentTarget.value);
+      }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newItem={
+          name : listName ,
+          age : listAge
+        };
+        
+        setStudents([...students, newItem]);
+        console.log([...students,newItem]);
+        }
     return ( 
     <div className="student">
-                <table>
+       <h2>Add a item</h2>
+       <form onSubmit={handleSubmit}>
+            <table>
+              <tbody>
+                <tr>
+                  <th><label>Name</label></th>
+                  <td>
+                    <input name="name" type="text" value={listName}   onChange={handletextChange} />
+                  </td>
+                </tr>
+
+                <tr>
+                  <th><label>Age</label></th>
+                  <td>
+                    <input name="age" type="number" value={listAge}  onChange={handlenumChange} />
+                  </td>
+                </tr>
+            
+              </tbody>
+            </table>
+            <button type="submit" >Submit</button>
+        
+        <hr />
+        <table>
         <tr>
           <th>Name</th>
           <th>Age</th>
@@ -23,7 +62,8 @@ export const Dang =()=>{
             </tr>
           )
         })}
-      </table>
+      </table> 
+      </form>
     </div>
     )
 };
